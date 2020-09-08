7684cfa7fed8349062e89c272acc3f5f59fed60f
SunJet Liu <sunjetliu@gmail.com>
2020-09-08 02:26:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,14 @@
 def reverse_each_word(sentence)
-arr = []
-word = []
-letter = []
-  word << sentence.split(" ")
-  word.each do |let|
-  letter << let.split("")
-puts letter
-  end
-  puts letter
+sentence.reverse.split.reverse.join " "
+# arr = []
+# word = []
+# letter = []
+#   word << sentence.split(" ")
+# 
+#   word.each do |let|
+#   letter << let.split("")
+#   puts letter
+#   end
+# 
+#   puts letter
 end
