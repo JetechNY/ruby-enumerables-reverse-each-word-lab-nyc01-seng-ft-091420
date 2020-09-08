e03ed8871c251c6841be9aa65a2ee63157c6c723
SunJet Liu <sunjetliu@gmail.com>
2020-09-08 03:00:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,28 +1,15 @@
 require 'pry'
 
-def reverse(word)
 
-reversed = " "
-  i= 0
-  while i < word.length
-    char = word[i]
-    reversed =   char +	reversed
-    i += 1
+def reverse_each_word(word)
+
+  words=sent.split(" ")
+  new_words = []
+  words.each { |word| new_words << word.reverse}
+  new_sent = new_words.join(" ")
+  return new_sent   
   end
-      return reversed
-end
 
-def reverse_each_word(sentence)
-
-# sentence.reverse.split.reverse.join " "
-arr = []
-
-wordarr = []
-letter = []
-  wordarr << sentence.split(" ")
-
-  wordarr.each do |thing|
-  arr = reverse(thing)
 
   end
   arr
