7b52951dbe684a9f09c0b5fdb58f3b968258482b
SunJet Liu <sunjetliu@gmail.com>
2020-09-08 02:56:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -21,9 +21,9 @@ def reverse(word)
 reversed = " "
   i= 0
   while i < word.length
-    char = word[i] 
+    char = word[i]
     reversed =   char +	reversed
     i += 1
-  end    
+  end
       return reversed
 end
